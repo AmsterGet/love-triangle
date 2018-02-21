1c986858947827893cbbb040f6596c8df0072832
AmsterGet <amsterget@gmail.com>
2018-02-21 21:28:04 +0300

    All love triangles founded

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
-/**
- * @param preferences - an array of integers. Indices of people, whom they love
- * @returns number of love triangles
- */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    let trianglesCount = 0;
+  preferences.forEach(function(item, index, array) {
+      let thirdLoversLove = array[array[item-1]-1]-1;
+      if (index===thirdLoversLove) {
+          trianglesCount++;
+      }
+  });
+  return Math.trunc(trianglesCount/3);
 };
